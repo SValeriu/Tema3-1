351fd1904aaf81ed8eb002e4da55a158f4013d0e
Fulgeanu <full.ioann@gmail.com>
2014-10-24 19:09:10 +0300

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
 		Car_class ford = new Car_class("Ford", "Taurus");
 		ford.printManufacturer();
 		ford.printModel();
+		
+		Car_class merc = new Car_class("Mercedes", "CLA 45AMG");
+		merc.printManufacturer();
+		merc.printModel();
 	}
 }
